a165f4bf55243d39d238f29ddec80f298549d188
TEERTH KOLHI <97418435+TEERTH-KOLHI@users.noreply.github.com>
2024-04-02 15:20:43 +0500

    Update index.js

--- Mid-Exam/index.js
+++ Mid-Exam/index.js
@@ -1,4 +1,3 @@
-
 let currentImageIndex = 0;
 const totalImages = 3; 
 
@@ -8,29 +7,4 @@ const showImage = (index) => {
   document.querySelectorAll('.slider-image').forEach(image => {
     image.style.display = 'none';
   });
-
-  
-  const images = document.querySelectorAll('.slider-image');
-  images[index].style.display = 'block';
-
-  
-  document.querySelectorAll('.thumbnail').forEach(thumbnail => {
-    thumbnail.classList.remove('active');
-  });
-  document.querySelector(`#thumbnail-${index}`).classList.add('active');
-};
-
-
-document.getElementById('next-btn').addEventListener('click', () => {
-  currentImageIndex = (currentImageIndex + 1) % totalImages;
-  showImage(currentImageIndex);
-});
-
-
-document.getElementById('prev-btn').addEventListener('click', () => {
-  currentImageIndex = (currentImageIndex - 1 + totalImages) % totalImages;
-  showImage(currentImageIndex);
-});
-
-
-showImage(currentImageIndex);
+}
